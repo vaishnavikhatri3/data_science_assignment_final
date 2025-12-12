6f35d88d399917a39d8074da84e37ce57a4d1f47
Vaishnavi Khatri <147296138+vaishnavikhatri3@users.noreply.github.com>
2025-12-12 16:32:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const sampleAssignments = [
     id: 1,
     title: 'Assignment 1 - MTECH IT INTERNAL 1 PAPER',
     video: 'https://youtube.com/playlist?list=PLSBMcQ-3aOgqoyCAG4lvi2VFBl8g0p0JV&si=ZPG0IFftB8ZW7Hp8',
-    pdf: 'https://docs.google.com/document/d/1lhl4Ijfgh3zcHMLWLNMPYjIMptTAdWILKOjWQAoCOus/edit?tab=t.0',
-    ppt: 'https://docs.google.com/presentation/d/1eBi28nPxEOfIcjKPQUXelC1ZzzG88pil/edit?usp=sharing&ouid=102752533725136234639&rtpof=true&sd=true',
+    pdf: 'https://docs.google.com/presentation/d/1eBi28nPxEOfIcjKPQUXelC1ZzzG88pil/edit?usp=sharing&ouid=102752533725136234639&rtpof=true&sd=true',
+    ppt: 'https://docs.google.com/document/d/1lhl4Ijfgh3zcHMLWLNMPYjIMptTAdWILKOjWQAoCOus/edit?tab=t.0',
     summary: 'This playlist contains all three videos of my Internal-1 Data Science paper solution (MTech IT).The paper is divided into three sections, and each video explains one full section in an easy and detailed way.Perfect for quick revision, last-minute study, and understanding concepts with clear logic.'
   },
   {
@@ -26,6 +26,7 @@ const sampleAssignments = [
     video: 'https://youtube.com/playlist?list=PLSBMcQ-3aOgqPtRy9TAE0mPBnObl9uBif&si=4e0jPQd_ENFcv8Ff',
     pdf: 'https://drive.google.com/file/d/1axNdlW6W-JnzNeayNmwYNM2KkToVkqjZ/view?usp=sharing',
     ppt: 'https://drive.google.com/file/d/1axNdlW6W-JnzNeayNmwYNM2KkToVkqjZ/view?usp=sharing',
+
     summary: 'This solution set covers all 40 Multiple Choice Questions (MCQs) of the Internal–III Examination for M.Tech (Information Technology) in a clear, easy, and well-structured manner. Each question is explained in simple language, ensuring quick understanding and effective revision.'
   },
   
@@ -34,7 +35,7 @@ const sampleAssignments = [
     title: 'Assignment 4 - MTECH  END SEMESTER PAPER',
     video: 'https://youtube.com/playlist?list=PLSBMcQ-3aOgpPmOLF3bBs_-9X7ajSIoCY&si=PSWlwCdlrPDUMftx',
     pdf: 'https://docs.google.com/document/d/1Ymql7cupcuPN4o2mdjt7zDSKa2GfGhElkt4Yh1_IyIU/edit?tab=t.0#heading=h.9f3w51giynvv',
-    ppt: 'https://docs.google.com/presentation/d/1Cfl_CZMMT3SZvTb-tBDfxSYOFCWSMX_t/edit?usp=sharing&ouid=102752533725136234639&rtpof=true&sd=true',
+    ppt: '',
     summary: 'This playlist is specially designed for the MTech End Semester Examination of Introduction to Data Science. It covers the entire syllabus in a structured and easy-to-understand manner'
   },
   {
@@ -54,14 +55,7 @@ const sampleAssignments = [
     ppt: 'https://docs.google.com/document/d/1c77BSQSx55J2bF_yqrh3RYYtsPJTvvf3TIIb-0PQgM8/edit?pli=1&tab=t.0',
     summary: 'This playlist covers all four sections of the MCA Second Internal Test for Introduction to Data Science. Each video includes accurate answers, clear explanations, and quick revision support for all 50 questions, including fill-in-the-blanks, Python commands, formulas, and conceptual topics.'
   },
-  {
-    id: 2,
-    title: 'Assignment 7 - E LAB',
-    video: '',
-    pdf: 'https://drive.google.com/file/d/1X6qujTOx1xiTYZ3zGZNoDn8_Pw8K07r3/view?usp=sharing',
-    ppt: 'https://docs.google.com/document/d/1c77BSQSx55J2bF_yqrh3RYYtsPJTvvf3TIIb-0PQgM8/edit?pli=1&tab=t.0',
-    summary: ''
-  }
+  
 ]
 
 export default function App() {
